f58dac7ea25016fd83d3a5af0ce75ae16ea6cd48
holonking <317200789@qq.com>
2018-04-01 01:19:26 +0800

    finished ShapeObject,tested,nextGrammar

--- ShapeGrammar/Core/ShapeObject.cs
+++ ShapeGrammar/Core/ShapeObject.cs
@@ -12,6 +12,8 @@ public class ShapeObject : MonoBehaviour {
         }
         set
         {
+            for (int i = 0; i < 3; i++)
+                if (value[i] == 0) value[i] = 1;
             transform.localScale = value;
         }
     }
@@ -26,7 +28,20 @@ public class ShapeObject : MonoBehaviour {
         }
     }
     public Meshable meshable;
+    public static Material DefaultMat
+    {
+        get
+        {
+            if(_defaultMat == null)
+            {
+                _defaultMat = Resources.Load("Mat0") as Material;
+            }
+            return _defaultMat;
+        }
+    }
 
+    public bool drawScope = true;
+    private static Material _defaultMat;
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
     // Use this for initialization
@@ -41,20 +56,19 @@ public class ShapeObject : MonoBehaviour {
 
     private void OnRenderObject()
     {
-        GLDrawScope();
+        if(drawScope && gameObject.activeSelf)
+            GLDrawScope();
     }
     Vector3[] makeBoxPoints()
     {
         Vector3[] opts = new Vector3[8];
         Vector3[] vects = Vects;
+        for (int i = 0; i < 3; i++)
+            vects[i] *= Size[i];
         Vector3 size = Size;
 
-        foreach(Vector3 v in vects)
-        {
-            print(v);
-        }
-
-        opts[0] = new Vector3(-0.5f,-0.5f,-0.5f);
+        //opts[0] = new Vector3(-0.5f,-0.5f,-0.5f);
+        opts[0] = transform.position;
         opts[1] = opts[0] + (vects[0]);
         opts[2] = opts[1] + (vects[2]);
         opts[3] = opts[0]+ (vects[2]);
@@ -71,11 +85,6 @@ public class ShapeObject : MonoBehaviour {
         Material mat = UserStats.LineMat;
         mat.SetPass(0);
         Vector3[] pts = makeBoxPoints();
-        GL.PushMatrix();
-        if (transform != null)
-        {
-            GL.MultMatrix(transform.localToWorldMatrix);
-        }
         GL.Begin(GL.LINES);
         GL.Color(Color.red);
         GL.Vertex(pts[0]);
@@ -104,32 +113,31 @@ public class ShapeObject : MonoBehaviour {
         GL.Vertex(pts[7]);
         GL.Vertex(pts[4]);
 
-        for (int i = 1; i < 4; i++)
+        for(int i =0;i<4;i++)
         {
             GL.Vertex(pts[i]);
-            GL.Vertex(pts[i + 4]);
+            GL.Vertex(pts[i+4]);
         }
 
-
         GL.End();
-        GL.PopMatrix();
     }
-    void SetMeshable(Meshable imeshable, Vector3? direction=null)
+    public void SetMeshable(Meshable imeshable, Vector3? direction=null)
     {
         meshable = imeshable;
         Vector3 vectu;
         if (direction.HasValue) vectu = direction.Value;
         else vectu = new Vector3(1, 0, 0);
         BoundingBox bbox = meshable.GetBoundingBox(vectu);
-        transform.position = bbox.position;
-        transform.localScale = bbox.size;
-        Vector3[] vects = bbox.vects;
-        transform.forward = vects[2];
-        transform.up = vects[1];
 
-        //Mesh mesh = meshable.GetMeshForm();
-        Mesh normalMesh = meshable.GetNormalizedMesh(bbox);
-        meshFilter.mesh = normalMesh;
+       
+
+        transform.position = bbox.vertices[0];
+        transform.LookAt(bbox.vertices[3]);
+        Size = bbox.size;
+        
+        Mesh mesh = meshable.GetNormalizedMesh(bbox);
+        GetComponent<MeshFilter>().mesh = mesh;
+        print("mesh.verticeCount=" + mesh.vertexCount.ToString());
 
     }
     public static ShapeObject CreateBasic()
@@ -138,6 +146,7 @@ public class ShapeObject : MonoBehaviour {
         ShapeObject so = o.AddComponent<ShapeObject>();
         so.meshFilter = o.AddComponent<MeshFilter>();
         so.meshRenderer = o.AddComponent<MeshRenderer>();
+        so.meshRenderer.material = DefaultMat;
         return so;
     }
     public static ShapeObject CreatePolygon(Vector3[] pts)
@@ -146,7 +155,21 @@ public class ShapeObject : MonoBehaviour {
         ShapeObject so = ShapeObject.CreateBasic();
         Vector3? ld = PointsBase.LongestDirection(pts);
         so.SetMeshable(pg, ld);
+        return so;
 
+    }
+    public static ShapeObject CreateExtrusion(Vector3[] pts, float d)
+    {
+        Vector3 magUp = new Vector3(0, d, 0);
+        Polygon pg = new Polygon(pts);
+        Form ext = pg.Extrude(magUp);
+        Debug.Log("ext.verticesCount" + ext.vertices.Length.ToString());
+        Debug.Log("ext.trianglesCount" + ext.triangles.Length.ToString());
+        Debug.Log("ext.polygonCount" + ext.components.Count.ToString());
+
+        ShapeObject so = ShapeObject.CreateBasic();
+        Vector3? ld = PointsBase.LongestDirection(pts);
+        so.SetMeshable(ext, ld);
         return so;
 
     }
